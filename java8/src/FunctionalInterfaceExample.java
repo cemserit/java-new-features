fb3c2629d4e5ef54b0e807561178fe259dbfff8b
Cemalettin Serit <cemalettin@evam.com>
2019-08-14 15:24:09 +0300

    Add functional interface example

---
+++ java8/src/FunctionalInterfaceExample.java
@@ -0,0 +1,11 @@
+/**
+ * Created by cemserit on 2019-08-14.
+ */
+@FunctionalInterface
+// A functional interface can have only one method.
+// If you have more than one method, you will get a compile error
+interface FunctionalInterfaceExample {
+	int square(int number);
+
+	//int cube(int number);
+}
